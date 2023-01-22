b6a73836d2943a55e36b78fb288b8e6652bdf5a9
Lesley <leskimuti@gmail.com>
2023-01-22 10:26:27 +0300

    Setting up App comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main>
+      <h1>Tenzies</h1>
+      <p className="main__text">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dices"></div>
+    </main>
+  );
 }
 
 export default App;
